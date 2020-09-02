de338a9d5e7bf9efb182ad7643e70835cc8019b9
Nehal Damania <nehaldamania@gmail.com>
2020-09-02 19:25:31 +0530

    Mindfort Agile Board

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { HomePageComponent } from './home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: HomePageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
